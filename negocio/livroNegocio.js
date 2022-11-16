f5c6f99acfb5d9eb5fa6dd228794870941ff32ed
SAULvaRGAS88 <saulvargascosta@hotmail.com>
2022-11-15 23:40:19 -0300

    Create CRUD Livro

--- negocio/livroNegocio.js
+++ negocio/livroNegocio.js
@@ -1,13 +1,13 @@
-const {validacaoLivro} = require('./validacao')
+const { validacaoLivro } = require('./validacao')
 const livroPersistence = require('../persistence/livroPersistence')
 
 async function inserireLivro(livro) {
-    if(validacaoLivro(livro)){
-        const livroInserido = await livroPersistence.inserireLivro(livro);
+    if (validacaoLivro(livro)) {
+        const livroInserido = await livroPersistence.insereLivro(livro);
         return livroInserido;
     }
     else {
-        throw { id: 400, mensagem: "Falta parametros"};
+        throw { id: 400, mensagem: "Falta parametros" };
     }
 }
 
@@ -15,7 +15,43 @@ async function listarLivros() {
     return await livroPersistence.listarLivros();
 }
 
-module.exports ={
+async function buscarLivroId(id) {
+    const livro = await livroPersistence.buscarLivroId(id);
+    if (!livro) {
+        throw { id: 404, mensagem: `Livro ${id} nao encontrado` };
+    }
+    return livro;
+}
+
+async function buscarLivroNome(nome) {
+    if (!nome) {
+        throw { id: 400, mensagem: "Falta parametro nome" };
+    }
+    return await livroPersistence.buscarLivroNome(nome);
+}
+
+async function atualizarLivro(id, livro) {
+    if (validacaoLivro(livro)) {
+        const livroAtualizar = await buscarLivroId(id);
+        if (livroAtualizar)
+            return await livroPersistence.atualizarLivro(id, livro);
+    }
+    else {
+        throw { id: 400, mensagem: "ALGUM PARAMETRO INVALIDO ...ARRUMA ISSO DEV..." };
+    }
+}
+
+async function deletarLivro(id) {
+    const livroDeletar = await buscarLivroId(id);
+    if (livroDeletar)
+        return await livroPersistence.deletarLivro(id);
+}
+
+module.exports = {
     inserireLivro,
-    listarLivros
+    listarLivros,
+    buscarLivroId,
+    buscarLivroNome,
+    atualizarLivro,
+    deletarLivro
 }
--- persistence/livroPersistence.js
+++ persistence/livroPersistence.js
@@ -1,5 +1,5 @@
-const {Client} = require('pg')
-const {conexao} = require('./conexao')
+const { Client } = require('pg')
+const { conexao } = require('./conexao')
 
 async function listarLivros() {
     const cliente = new Client(conexao)
@@ -9,7 +9,63 @@ async function listarLivros() {
     return res.rows
 }
 
+async function insereLivro(livro) {
+    const cliente = new Client(conexao)
+    await cliente.connect()
+    const res = await cliente.query('INSERT INTO livro (nome, autor, editora, ano) VALUES ($1,$2,$3,$4) RETURNING *', [
+        livro.nome,
+        livro.autor,
+        livro.editora,
+        livro.ano
+    ])
+    await cliente.end()
+    return res.rows[0]
+}
+
+
+async function buscarLivroId(id) {
+    const cliente = new Client(conexao)
+    await cliente.connect();
+    const res = await cliente.query('SELECT * FROM livro WHERE id=$1', [id]);
+    await cliente.end();
+    return res.rows[0];
+}
+
+async function buscarLivroNome(nome) {
+    const cliente = new Client(conexao)
+    await cliente.connect();
+    const res = await cliente.query('SELECT * FROM livro WHERE nome=$1', [nome]);
+    await cliente.end();
+    return res.rows;
+}
+
+async function atualizarLivro(id, livro) {
+    const cliente = new Client(conexao)
+    await cliente.connect();
+    const res = await cliente.query('UPDATE livro SET nome=$1, autor=$2, editora=$3, ano=$4 WHERE id=$5 RETURNING *', [
+        livro.nome,
+        livro.autor,
+        livro.editora,
+        livro.ano,
+        id
+    ]);
+    await cliente.end();
+    return res.rows[0]
+}
+
+async function deletarLivro(id) {
+    const cliente = new Client(conexao)
+    await cliente.connect();
+    const res = await cliente.query('DELETE FROM livro WHERE id=$1 RETURNING *',[id]);
+    await cliente.end();
+    return res.rows[0];
+}
+
 module.exports = {
     listarLivros,
-
+    insereLivro,
+    buscarLivroId,
+    buscarLivroNome,
+    atualizarLivro,
+    deletarLivro
 }
